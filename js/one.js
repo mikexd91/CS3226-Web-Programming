0d65d6446bb7fec7cad193e3d3e50d38ebd2c55d
Acrobat622 <86.ed.liu@gmail.com>
2017-01-26 16:03:31 +0800

    dirty commit

--- js/one.js
+++ js/one.js
@@ -1,25 +1,17 @@
-var data = [
-		[1, 1, 1, 1, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'], // mc
-		[0, 0, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],  // tc
-		[1, 1.5, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],  //hw
-		[0, 1, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],  //ps
-		[0, 1, 1, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],  //ks
-		[2, 1, 1, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']  //ac
-	];
 
-createTable("abyss",data);
 
 function createTable(name, data) {
-	var component = ["Mini Contests", "Team Contests", "Homework", "Problem Bs", 
+	component = ["Mini Contests", "Team Contests", "Homework", "Problem Bs", 
 					"Kattis Sets", "Achievements"];
-	var head = ["Components", "Sum", 01, 02, 03, 04, 05, 06, 07, 08, 09, 10,
+	head = ["Components", "Sum", 01, 02, 03, 04, 05, 06, 07, 08, 09, 10,
 				11, 12];
-	var field = ["mc", "tc", "hm", "ps", "ks", "ac"];
+	field = ["mc", "tc", "hm", "ps", "ks", "ac"];
 	
+	var summary = [name];
 	
 	var i, j;
 	
-	var tbody=$('table.'+name);
+	var tbody=$("table."+name);
 	// create table head
 	tbody.append("<thead><tr>");
 	for (i in head)
@@ -38,15 +30,34 @@ function createTable(name, data) {
 			row = row.concat("<td>" + data[i][j] + "</td>");
 		}
 		row = row.concat("</tr>");
-		console.log(row);
 		// insert sum cell
 		var position = getPosition(row, "<td>", 2);
-		row = [row.slice(0, position), "<td>" + sum + "</td>", row.slice(position)].join('');
-		console.log(row.slice(0, position));
+   		row = [row.slice(0, position), "<td id=\"" + field[i] + "\">" + sum + "</td>", row.slice(position)].join('');
+		summary.push(sum);
 		tbody.append(row);
 	}
+
+	return summary;
 }
 
 function getPosition(string, subString, index) {
    return string.split(subString, index).join(subString).length;
 }
+
+      	
+function hightlightCrosses() {
+  var topVal = 0;
+  var topCell = 0;
+  var cols = $("#studentTable").find("tbody").find("td");
+  console.log($("#studentTable"));
+  var colLen = cols.length;
+    
+	for (j = 0; j < colLen; j++){                
+    var cellVal = cols[j].innerHTML; 
+
+    if (cellVal == "x") {
+      $(cols[j]).addClass("pinkCol");
+    } 
+  }
+}
+
