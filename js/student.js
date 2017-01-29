a7358f47ea4bc460e037f8f426ca5a3c61e76a37
mikexd91 <mike.xunda@gmail.com>
2017-01-30 00:42:11 +0800

    fixed bug in student table sum column not showing properly

--- js/student.js
+++ js/student.js
@@ -16,13 +16,8 @@ function createTable(name, data) {
 	var tbody=$("table."+name);
 	// create table head
 	tbody.append("<thead><tr>");
-	for (i in studentTableHeadTitles){
-		if(i==0 || i==1){
-			tbody.append("<th>"+studentTableHeadTitles[i]+"</th>");
-		}else{
-			tbody.append("<th class='unused'>"+studentTableHeadTitles[i]+"</th>");
-		}
-	}
+	for (i in studentTableHeadTitles)
+		tbody.append("<th>"+studentTableHeadTitles[i]+"</th>");
 	tbody.append("</tr></thead>");
 	//console.log(tbody);
 
@@ -33,26 +28,40 @@ function createTable(name, data) {
 		row = row.concat("<tr class=\"" + studentTableField[i] + "\">"); // component cell
 		row = row.concat("<td>" + studentTableFieldTitles[i] + "</td>");
 		var sum = 0;
-		row = row.concat("<td id='addsum'>" + sum + "</td>");
 		for (j = 0; j < data[i].length; j++) {
 			if (typeof(data[i][j]) == "number")
 				sum += data[i][j];
-			row = row.concat("<td class ='unused'>" + data[i][j] + "</td>");
+			row = row.concat("<td>" + data[i][j] + "</td>");
 		}
-		$('#addsum').html(sum);
 		row = row.concat("</tr>");
-       
 		// insert sum cell
-		//var position = getPosition(row, "<td>", 2);
-   		//row = [row.slice(0, position), "<td id=\"" + studentTableField[i] + "\">" + sum + "</td>", row.slice(position)].join('');
+		var position = getPosition(row, "<td>", 2);
+   		row = [row.slice(0, position), "<td id=\"" + studentTableField[i] + "\">" + sum + "</td>", row.slice(position)].join('');
 		summary.push(sum);
 		tbody.append(row);
 	}
 	tbody.append("</tbody>");
-
+    hideTable(name);
 	return summary;
 }
+function hideTable(name){
+	var i = 11;
+	//$('table.'+name).find('td:nth-child(16),th:nth-child(16)').addClass('unused');
+	$('table.'+name).find('td:nth-child(15),th:nth-child(15)').addClass('unused');
+	$('table.'+name).find('td:nth-child(14),th:nth-child(14)').addClass('unused');
+	$('table.'+name).find('td:nth-child(13),th:nth-child(13)').addClass('unused');
+	$('table.'+name).find('td:nth-child(12),th:nth-child(12)').addClass('unused');
+	$('table.'+name).find('td:nth-child(11),th:nth-child(11)').addClass('unused');
+	$('table.'+name).find('td:nth-child(10),th:nth-child(10)').addClass('unused');
+	$('table.'+name).find('td:nth-child(9),th:nth-child(9)').addClass('unused');
+	$('table.'+name).find('td:nth-child(8),th:nth-child(8)').addClass('unused');
+	$('table.'+name).find('td:nth-child(7),th:nth-child(7)').addClass('unused');
+	$('table.'+name).find('td:nth-child(6),th:nth-child(6)').addClass('unused');
+	$('table.'+name).find('td:nth-child(5),th:nth-child(5)').addClass('unused');
+	$('table.'+name).find('td:nth-child(4),th:nth-child(4)').addClass('unused');
+	$('table.'+name).find('td:nth-child(3),th:nth-child(4)').addClass('unused');	
 
+}
 function getPosition(string, subString, index) {
    return string.split(subString, index).join(subString).length;
 }
